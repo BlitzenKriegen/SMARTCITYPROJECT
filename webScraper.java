10505b092d62764c92b43b4a471b21abf2f02228
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-04-03 18:42:18 -0600

    Baseline Search Creator
    TODO: 
    -Trim to only results
    -Looping functionality
    -Adblocker?

--- webScraper.java
+++ webScraper.java
@@ -3,14 +3,18 @@
  */
 package SeleniumScraper;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 /**
  * @author kiril
@@ -19,8 +23,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class webScraper {
     static String driverName = "webdriver.gecko.driver";
     //This one is user specific. Replace with user's path of the geckodriver.
-    static String driverPath = "C:\\Users\\kiril\\eclipse-workspace\\WebScraper\\Resources\\geckodriver.exe";
-    static char newline = '\n';
+    static String driverPath = "C:\\SeleniumDocs\\geckodriver.exe";
+    static String ScreenShotPath = "C:\\SeleniumDocs\\ScreenShot";
+
     /**
      * @param args
      */
@@ -32,28 +37,42 @@ public class webScraper {
     }
 
     private static void searchMaps(String tst) {
-        char debugLoop = ' ';
-        Scanner sc = new Scanner(System.in);
         System.setProperty(driverName, driverPath);
-        Results searchResult = new Results();
-        WebDriver driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
 
         initSearch(driver);
         performSearch(tst,driver);
 
-        getData(searchResult,driver);
-
-        while(debugLoop != newline) {
-            debugLoop = sc.next().charAt(0);
-        }
+        getData(driver);
 
         driver.quit();
-        sc.close();
     }
 
-    public static void getData(Results searchResult, WebDriver driver) {
-        WebElement ResultBox = driver.findElement(By.id("Results For"));
-        WebElement Single = ResultBox.findElement(By.name("aria-label"));
+    public static void getData(WebDriver driver) {
+        File searchResult = new File(ScreenShotPath);
+        waitForLoad();
+        TakesScreenshot screenshot = (TakesScreenshot)driver;
+
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFileToDirectory(source, searchResult);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return;
+    }
+
+    private static void waitForLoad() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
         return;
     }
 
